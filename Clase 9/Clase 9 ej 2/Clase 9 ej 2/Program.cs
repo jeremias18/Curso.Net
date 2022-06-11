5f828d8e8bda61c96514d19c4c6642a5aeaf5a1a
jeremias18 <jeremiaspaladini@gmail.com>
2022-06-11 00:50:36 -0300

    Bingo y terminados otros projectos

---
+++ Clase 9/Clase 9 ej 2/Clase 9 ej 2/Program.cs
@@ -0,0 +1,108 @@
+﻿//1)    Cartón de 3 filas por 9 columnas
+//2)    El cartón debe tener 15 números y 12 espacios en blanco
+//3)    Cada fila debe tener 5 números
+//4)    Cada columna debe tener 1 o 2 números
+//5)    Ningún número puede repetirse
+//6)    La primer columna contiene los números del 1 al 9, la segunda del 10 al 19, la tercera del 20 al 29,
+//      así sucesivamente hasta la última columna la cual contiene del 80 al 90
+//7)    Mostrar el carton por pantalla
+int columna = 9; int fila = 3;
+int numTotal = 0;
+int[,] cartonBingo = new int[fila, columna];
+Random Random = new Random();
+
+cargarPrimerCarton();
+terminarCarton();
+mostrarCarton();
+
+
+
+
+void cargarPrimerCarton() {
+    for (int columna = 0; columna < cartonBingo.GetLength(1); columna++) {
+        int numMin = (columna * 10); int numMax = ((columna + 1) * 10) - 1;
+        int random = Random.Next(numMin, numMax);
+        int posRandom = Random.Next(0, 2);
+        cartonBingo[posRandom, columna] = random;
+        numTotal++;
+    }
+    Console.WriteLine("La primer carga termino. Fueron:" + numTotal);
+}
+
+void terminarCarton() {
+    int numPorCargar = 6;
+    int num = 0;
+    while(num < numPorCargar ) {
+        for (int columna = 0; columna < cartonBingo.GetLength(1); columna++) {
+            int numMin = (columna * 10); int numMax = ((columna + 1) * 10) - 1;
+            int random = Random.Next(numMin, numMax);
+            if (probabilidadNum() == true && num < numPorCargar) {
+                bool numEvitado = comprobarRandom(random, columna);
+                if (numEvitado == false) {
+                    num++;
+                    numTotal++;
+                }
+            }
+        }
+    }
+    Console.WriteLine("Se cargó: " + num + " numero");
+    Console.WriteLine("Cargados: " + numTotal);
+}
+
+
+void mostrarCarton() {
+    for (int i = 0; i < cartonBingo.GetLength(0); i++) {
+        for (int j = 0; j < cartonBingo.GetLength(1); j++) {
+            Console.Write(cartonBingo[i, j] + " | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Boolean comprobarRandom(int pRandom, int pColumna) {
+    Boolean repetido = false; int numPorFila = 0;
+    for (int j = 0; j < cartonBingo.GetLength(0); j++) {
+        if (pRandom == cartonBingo[j, pColumna]) {
+            repetido = true;
+            return repetido;
+        }
+    }
+
+
+    for (int j = 0; j < cartonBingo.GetLength(0); j++) {
+        if (cartonBingo[j, pColumna] != 0) {
+            numPorFila++;
+            if (numPorFila <= 1) {
+                if (cartonBingo[j, pColumna] > pRandom) {
+                    if (j == 0) {
+                        cartonBingo[(j + 1), pColumna] = cartonBingo[j, pColumna];
+                        cartonBingo[j, pColumna] = pRandom;
+                        return repetido;
+                    } else {
+                        cartonBingo[(j - 1), pColumna] = pRandom;
+                        return repetido;
+                    }
+                } else {
+                    if (j == 2) {
+                        cartonBingo[(j - 1), pColumna] = cartonBingo[j, pColumna];
+                        cartonBingo[j, pColumna] = pRandom;
+                        return repetido;
+                    } else {
+                        cartonBingo[(j + 1), pColumna] = pRandom;
+                        return repetido;
+                    }
+                }
+            }
+        }
+    }
+    return repetido;
+}
+
+Boolean probabilidadNum() {
+    int prob = Random.Next(0, 100);
+    if (prob > 49) {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ Clase9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿Random aleatorio = new Random();
+int intentosPosibles = 3;
+Boolean encontrado = false;
+int numeroGanador = aleatorio.Next(0,30);
+
+Console.WriteLine("Bienvenido al juego");
+Console.WriteLine("Deberá adivinar un número entre 0 y 30");
+
+while ((encontrado == false) && (intentosPosibles > 0)){
+    Console.WriteLine("Intentos disponibles: " + intentosPosibles);
+    Console.WriteLine("Ingrese su numero: ");
+    int numeroUsuario = int.Parse(Console.ReadLine());
+    if (validarNumeroIngresado(numeroUsuario)) {
+        if (numeroUsuario == numeroGanador) {
+            encontrado = true;
+        } else {
+            comprobarNumero(numeroUsuario);
+            //Console.WriteLine("El numero ingresado no es correcto, vuelva a intentarlo.");
+            intentosPosibles--;
+        }
+    } else {
+        Console.WriteLine("El número ingresado está fuera de rango");
+    }
+    espaciado();
+}
+
+if (encontrado == true) {
+    Console.WriteLine("Felicidades! ganaste el juego. Reclamale el premio al creador del juego.");
+} else {
+    Console.WriteLine("Perdiste! No te pongas mal, paga $100 y volvé a intentar.");
+    Console.WriteLine("El número correcto era: " + numeroGanador);
+}
+
+Boolean validarNumeroIngresado(int pNum) {
+    if ((pNum < 100) || (pNum > 0)) {
+        return true;
+    } else {
+        return false;
+    }
+} 
+
+void espaciado() {
+    for(int i = 0; i < 4; i++) {
+        Console.WriteLine("");
+    }
+}
+
+void comprobarNumero(int pNum) {
+    if (pNum < numeroGanador) {
+        Console.WriteLine("Es un número mayor...");
+    } else {
+        Console.WriteLine("Es un número menor...");
+    }
+}
