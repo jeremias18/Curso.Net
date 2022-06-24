7ef0ed8c419c1c69af0322b72320d81be815155f
jeremias18 <jeremiaspaladini@gmail.com>
2022-06-24 00:59:32 -0300

    Creado ejercicio de billetera utilizando POO

---
+++ Clase 13/Clase 13/Models/Billetera.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_13.Models {
+    internal class Billetera {
+
+        //Propiedades
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        //Constructor
+        public Billetera() { }
+
+        public Billetera(int billete10, int billete20, int billete50, int billete100, int billete500, int billete1000) {
+            BilletesDe10 = billete10;
+            BilletesDe20 = billete20;
+            BilletesDe50 = billete50;
+            BilletesDe100 = billete100;
+            BilletesDe500 = billete500;
+            BilletesDe1000 = billete1000;
+        }
+
+        //Métodos
+        public decimal Total() {
+            decimal total = 0;
+            total += 10 * BilletesDe10;
+            total += 20 * BilletesDe20;
+            total += 50 * BilletesDe50;
+            total += 100 * BilletesDe100;
+            total += 500 * BilletesDe500;
+            total += 1000 * BilletesDe1000;
+            return total;
+        }
+
+        public Billetera Combinar(Billetera billetera) {
+            var NuevaBilletera = new Billetera();
+            NuevaBilletera.BilletesDe10 = (billetera.BilletesDe10 + BilletesDe10);
+            NuevaBilletera.BilletesDe20 = (billetera.BilletesDe10 + BilletesDe20);
+            NuevaBilletera.BilletesDe50 = (billetera.BilletesDe10 + BilletesDe50);
+            NuevaBilletera.BilletesDe100 = (billetera.BilletesDe10 + BilletesDe100);
+            NuevaBilletera.BilletesDe500 = (billetera.BilletesDe10 + BilletesDe500);
+            NuevaBilletera.BilletesDe1000 = (billetera.BilletesDe10 + BilletesDe1000);
+            return NuevaBilletera;
+        }
+
+        public void Vaciar() {
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+        }
+
+    }
+}
---
+++ Clase 13/Clase 13/Program.cs
@@ -0,0 +1,23 @@
+﻿using Clase_13.Models;
+
+namespace Clase_13 {
+    internal class Program {
+        static void Main(string[] args) {
+            var billetera1 = new Billetera(1, 1, 1, 1, 1, 1);
+            var billetera2 = new Billetera(3, 3, 3, 3, 3, 3);
+            var billeteraCombinada = new Billetera();
+
+
+            Console.WriteLine(billetera1.Total());
+            Console.WriteLine(billetera2.Total());
+            billeteraCombinada = billetera1.Combinar(billetera2 as Billetera);
+            billetera1.Vaciar();
+            billetera2.Vaciar();
+
+            Console.WriteLine(billeteraCombinada.Total());
+            Console.WriteLine(billetera1.Total());
+            Console.WriteLine(billetera2.Total());
+        }
+
+    }
+}
