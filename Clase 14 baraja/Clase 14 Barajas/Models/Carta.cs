755c3e2c2b8d3ae608dc4734a3ddb44db780b54d
jeremias18 <jeremiaspaladini@gmail.com>
2022-06-25 02:39:22 -0300

    Continuacion clase 14

---
+++ Clase 14 baraja/Clase 14 Barajas/Models/Carta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_14_Barajas.Models {
+    public class Carta {
+
+        public int Numero;
+        public string Palo = "";
+    }
+
+}
---
+++ Clase 14 baraja/Clase 14 Barajas/Program.cs
@@ -0,0 +1,47 @@
+﻿using Clase_14_Barajas.Models;
+
+
+var Baraja = new List<Carta>();
+Baraja = CrearBaraja();
+
+Console.WriteLine("Total de cartas en la baraja: " + Baraja.Count);
+foreach(Carta carta in Baraja) {
+    Console.WriteLine("Palo: " + carta.Palo);
+    Console.WriteLine("Número: " + carta.Numero);
+    Console.WriteLine();
+}
+
+
+List<Carta> CrearBaraja() {
+    var cartas = new List<Carta>();
+    for (int i = 0; i < 4; i++) {
+        for (int j = 1; j < 11; j++) {
+            var carta = new Carta();
+            carta.Palo = CrearPalo(i);
+            carta.Numero = CrearNumero(j);
+            cartas.Add(carta);
+        }
+    }
+    return cartas;
+}
+
+int CrearNumero(int j) {
+    var numero = 0;
+    if (j < 8) {
+        numero = j;
+    } else {
+        numero = j + 2;
+    }
+    return numero;
+}
+
+string CrearPalo(int i) {
+    string palo = "";
+    switch (i) {
+        case 0: palo = "Espada"; break;
+        case 1: palo = "Basto"; break;
+        case 2: palo = "Oro"; break;
+        case 3: palo = "Copa"; break;
+    }
+    return palo;
+}
